f1bf590b5c6deb0e6f10b02da163369093f3ef6b
zhouandr <>
2021-10-21 20:25:13 +0800

    update filter for nodejs

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,5 +1,5 @@
 var AWS = require("aws-sdk");
-AWS.config.getCredentials(function(err) {
+AWS.config.getCredentials(function (err) {
   if (err) {
     console.log(err.stack); // credentials not loaded
   } else {
@@ -9,30 +9,41 @@ AWS.config.getCredentials(function(err) {
 
 // Configuration: Put your region, tag key and value here
 AWS.config.update({ region: 'ap-northeast-1' })
-var keystr = "20210510";
-var valuestr = "afternoon";
-var instanceFilter = [{'Name':'launch-time', 'Values':['2021-05-26']}];
-//var instanceFilter = [];
+var keystr = "map-migrated";
+var valuestr = "d-server-20211021";
+// Filter 参考 https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html
+var instanceFilter1 = [{ 'Name': 'launch-time', 'Values': ['2021-05-26'] }];
+var instanceFilter2 = {
+  Filters: [
+    {
+      Name: "instance-type",
+      Values: [
+        "t2.micro"
+      ]
+    }
+  ]
+};
 
 // Tag EC2 instances
 var ec2 = new AWS.EC2();
-var params = instanceFilter;
+var params = {};
+
 var instanceIds = [];
-ec2.describeInstances(params, function(err, data) {
+ec2.describeInstances(params, function (err, data) {
   if (err) {
     console.log(err, err.stack); // an error occurred
   } else {
-    //console.log(data);           // successful response
+    console.log(data);           // successful response
     data.Reservations.forEach(function (resv) {
-      //console.log(resv);
-      resv.Instances.forEach(function (inst){
+      console.log(resv);
+      resv.Instances.forEach(function (inst) {
         //console.log(inst.InstanceId);
         instanceIds.push(inst.InstanceId);
       })
     });
 
     console.log(instanceIds);
-    //createTags(instanceIds, keystr, valuestr);
+    createTags(instanceIds, keystr, valuestr);
   }
 
 });
@@ -52,13 +63,13 @@ function createTags (instanceIds, key, value) {
     Resources: instanceIds,
     Tags: [
       {
-        Key: key, 
+        Key: key,
         Value: value
       }
     ]
   };
 
-  ec2.createTags(params, function(err, data) {
+  ec2.createTags(params, function (err, data) {
     if (err) {
       console.log(err, err.stack); // an error occurred
     } else {
